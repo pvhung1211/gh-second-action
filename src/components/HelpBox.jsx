092e7792926dc39a55664deb027d87cbe0b887ba
pviethung <pvhung1211@gmail.com>
2026-02-05 16:48:14 +0700

    change heading level from h3 to h5 in HelpBox component

--- src/components/HelpBox.jsx
+++ src/components/HelpBox.jsx
@@ -3,7 +3,7 @@ import './HelpBox.css';
 function HelpBox({ title, text }) {
   return (
     <article className="help-box">
-      <h3>{title}</h3>
+      <h5>{title}</h5>
       <p>{text}</p>
     </article>
   );
